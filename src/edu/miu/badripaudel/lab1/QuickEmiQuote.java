49ad97fe725351e29ba9c380791244216ff45d95
Badri Paudel <badripaudel77@gmail.com>
2025-09-02 15:33:44 -0500

    Program Simplified

--- src/edu/miu/badripaudel/lab1/QuickEmiQuote.java
+++ src/edu/miu/badripaudel/lab1/QuickEmiQuote.java
@@ -9,50 +9,23 @@ import java.util.Random;
  * Program name: <b>“Quick EMI Quote Calculator”</b>
  */
 public class QuickEmiQuote {
-    int principalAmount;
-    int tenure;
-    double yearlyInterestRate;
 
-    Random random;
-
-    public QuickEmiQuote() {
-        random = new Random();
-    }
-
-    private void calculateEmi() {
-        int p = getPrincipalAmount(5000, 25000);
-        double apr = getYearlyInterestRate(3.5, 11.5);
-        int n = getTenure(6, 36);
+    public static void main(String[] args) {
+        Random random = new Random();
+        int p = random.nextInt(5000, 25000+1);
+        // Double.MIN_VALUE is added because random number never produce exactly 1
+        double apr = random.nextDouble(11.48, 11.5 + Double.MIN_VALUE);
+        int n = random.nextInt(6, 36+1);
         // monthly interest rate, r
         double r = apr / (12 * 100);
 
         double emi = p * r * Math.pow((1+r), n) / (Math.pow(1+r, n)-1);
         double totalPayment = emi * n;
         double totalInterest = totalPayment - p;
-        System.out.println("Total Principal = " + p + ", Annual Interest Rate = " + String.format("%.2f", apr) + " and Tenure = " + n);
+        System.out.println("Details: \n\tTotal Principal = " + p + ", Annual Interest Rate = " + String.format("%.2f", apr) + " and Tenure = " + n);
         System.out.println("Total Payment = " + String.format("%.2f",totalPayment));
         System.out.printf("Total Interest = %.2f\n", totalInterest);
         System.out.println("EMI (in whole figure) " + Math.round(emi));
-        System.out.println("Emit (truncated figure) " + (int) emi);
-    }
-
-    private int getPrincipalAmount(int lower, int upper) {
-        principalAmount = random.nextInt(lower, upper+1);
-        return principalAmount;
-    }
-
-    private int getTenure(int lower, int upper) {
-        tenure = random.nextInt(lower, upper+1);
-        return tenure;
-    }
-
-    private double getYearlyInterestRate(double lower, double upper) {
-        yearlyInterestRate = lower + (upper - lower) * Math.random(); //Khanh - fixed random double
-        return yearlyInterestRate;
-    }
-
-    public static void main(String[] args) {
-        QuickEmiQuote quickEmiQuote = new QuickEmiQuote();
-        quickEmiQuote.calculateEmi();
+        System.out.println("EMI (truncated figure) " + (int) emi);
     }
 }
