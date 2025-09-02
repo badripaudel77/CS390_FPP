0dcbc916413e24e80265ca69bd1db8a1d1bccb47
Nguyen Khanh Tran <mrkhanhvd4@gmail.com>
2025-09-01 22:04:21 -0500

    [Khanh] Addd Prog6 - Find second minimum

---
+++ src/edu/miu/badripaudel/lab1/Prog6.java
@@ -0,0 +1,33 @@
+package edu.miu.badripaudel.lab1;
+
+/**
+ * @author Nguyen Khanh, Tran (ID: 619956)
+ * @since 9/2/2025
+ * <p>This is the assignment for lab 1, FPP course, MIU, IA</p>
+ * Program name: <b>“Find Second Minimum”</b>
+ */
+public class Prog6 {
+    public static void main(String[] args) {
+        int[] input = { 20, 21, 38, 45, 0, 12, 18, 69, 39, 100, 0, 22 };
+        secondMin(input);
+    }
+
+    public static void secondMin(int[] arrayOfInts){
+        if (arrayOfInts == null || arrayOfInts.length == 0){
+            System.out.println("Array is null or empty");
+            return;
+        }
+
+        int min = arrayOfInts[0];
+        int second = arrayOfInts[0];
+        for (int i = 1; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i] < min) {
+                min = arrayOfInts[i];
+            }
+            else if (arrayOfInts[i] < second && arrayOfInts[i] > min) {
+                second = arrayOfInts[i];
+            }
+        }
+        System.out.println("secondMin: " + second);
+    }
+}
