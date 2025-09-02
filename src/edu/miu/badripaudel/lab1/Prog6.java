a63c2a1d8be22cb4a9f2124773cba49a85d4ec01
Nguyen Khanh Tran <mrkhanhvd4@gmail.com>
2025-09-01 22:56:34 -0500

    [Khanh] Added Prog6 - Bill gen

--- src/edu/miu/badripaudel/lab1/Prog6.java
+++ src/edu/miu/badripaudel/lab1/Prog6.java
@@ -1,33 +1,53 @@
 package edu.miu.badripaudel.lab1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+
 /**
  * @author Nguyen Khanh, Tran (ID: 619956)
  * @since 9/2/2025
  * <p>This is the assignment for lab 1, FPP course, MIU, IA</p>
- * Program name: <b>“Find Second Minimum”</b>
+ * Program name: <b>“Electricity Bill Generator”</b>
  */
 public class Prog6 {
-    public static void main(String[] args) {
-        int[] input = { 20, 21, 38, 45, 0, 12, 18, 69, 39, 100, 0, 22 };
-        secondMin(input);
-    }
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void secondMin(int[] arrayOfInts){
-        if (arrayOfInts == null || arrayOfInts.length == 0){
-            System.out.println("Array is null or empty");
-            return;
-        }
+    public static void main(String[] args) throws IOException {
 
-        int min = arrayOfInts[0];
-        int second = arrayOfInts[0];
-        for (int i = 1; i < arrayOfInts.length; i++) {
-            if (arrayOfInts[i] < min) {
-                min = arrayOfInts[i];
+        String isExit;
+        do {
+            System.out.print("Enter user type (R/C/I):");
+            String userType = reader.readLine().trim();
+            System.out.print("Enter units consumed:");
+            int unitsConsumed = Integer.parseInt(reader.readLine().trim());
+
+            BigDecimal billAmt = new BigDecimal(0);
+            billAmt = switch (userType.toUpperCase()) {
+                case "R" -> new BigDecimal(unitsConsumed).multiply(new BigDecimal("0.12"));
+                case "C" -> new BigDecimal(unitsConsumed).multiply(new BigDecimal("0.2"));
+                case "I" -> new BigDecimal(unitsConsumed).multiply(new BigDecimal("0.35"));
+                default -> billAmt;
+            };
+
+            //Print bill
+            System.out.println("\n=== Electricity Bill ===");
+            System.out.println("User Type:" + userType);
+            System.out.println("Units Consumed:" + unitsConsumed);
+            System.out.printf("Bill Amount:" + String.format("$%.2f", billAmt) + "\n");
+
+            //Ask
+            do {
+                System.out.print("\nDo you want to calculate another bill? (Y or N):  ");
+                isExit = reader.readLine();
+            } while (!("Y".equalsIgnoreCase(isExit) || "N".equalsIgnoreCase(isExit)));
+
+            if("N".equalsIgnoreCase(isExit)) {
+                System.out.println("Goodbye! Thanks for using the Electricity Bill Generator.");
             }
-            else if (arrayOfInts[i] < second && arrayOfInts[i] > min) {
-                second = arrayOfInts[i];
-            }
-        }
-        System.out.println("secondMin: " + second);
+            System.out.println("\n");
+
+        } while ("Y".equalsIgnoreCase(isExit));
     }
 }
