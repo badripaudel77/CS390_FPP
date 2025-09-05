323bc3265b024398586db7ad78ac357f1a324625
Nguyen Khanh Tran <mrkhanhvd4@gmail.com>
2025-09-05 18:52:21 -0500

    [Khanh] Add Problem1.java

---
+++ src/edu/miu/lab3/problem1/Problem1.java
@@ -0,0 +1,20 @@
+package edu.miu.lab3.problem1;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        System.out.println(findMini("akel"));
+        System.out.println(findMini("khanh"));
+    }
+
+    private static char findMini(String s) {
+        if (s == null || s.isBlank())
+            return (char) Integer.MAX_VALUE;
+        char c = s.charAt(0);
+        char next = findMini(s.substring(1));
+        if (c < next) {
+            return c;
+        } else {
+            return next;
+        }
+    }
+}
