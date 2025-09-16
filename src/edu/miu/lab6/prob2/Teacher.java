75dff14b5e1bcbf4af125007c5c5a5e4b5c55886
Badri Paudel <badripaudel77@gmail.com>
2025-09-15 20:36:49 -0500

    Problem 2 implemented

--- src/edu/miu/lab6/prob2/Teacher.java
+++ src/edu/miu/lab6/prob2/Teacher.java
@@ -1,6 +1,6 @@
-package prob2;
+package edu.miu.lab6.prob2;
 
-public final class Teacher{
+public final class Teacher implements EmployeeData {
 	private double bonus;
 	private String name;
 	private double salary;
@@ -9,7 +9,7 @@ public final class Teacher{
 		this.salary = salary;
 		this.bonus = bonus;
 	}
-	
+    @Override
 	public double getSalary() {
 		return salary + bonus;
 	}
