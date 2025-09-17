990cfe41d6e9d2d11d760fe614cb6462c4ab7259
Nguyen Khanh Tran <mrkhanhvd4@gmail.com>
2025-09-17 14:22:44 -0500

    [Khanh] Solved Prob3.java

---
+++ src/edu/miu/lab7/prob3/BrowserHistory.java
@@ -0,0 +1,40 @@
+package edu.miu.lab7.prob3;
+
+import java.util.Stack;
+
+public class BrowserHistory {
+    private final Stack<String> backStack;
+    private final Stack<String> forwardStack;
+    private String current;
+    public BrowserHistory(String s) {
+        current = s;
+        backStack = new Stack<>();
+        forwardStack = new Stack<>();
+    }
+
+    public void visit(String s) {
+        backStack.push(current);
+        current = s;
+        System.out.println("Visited: " + s);
+    }
+
+    public void back() {
+        if(backStack.empty()) {
+            System.out.println("No history to go back to.");
+            return;
+        }
+        forwardStack.push(current);
+        current = backStack.pop();
+        System.out.println("Back to: " + current);
+    }
+
+    public void forward() {
+        if(forwardStack.empty()) {
+            System.out.println("No forward history.");
+            return;
+        }
+        backStack.push(current);
+        current = forwardStack.pop();
+        System.out.println("Forward to: " + current);
+    }
+}
